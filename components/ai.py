e41fb0dba62d760cdb9ee2b4b5f9e95e4f0a52be
Alexei Korol <korolalexei@gmail.com>
2023-10-12 22:42:20 +0400

    Added get_path_to method to BaseAI and removed unnecessary code from HostileEnemy.

--- components/ai.py
+++ components/ai.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import List, Tuple, TYPE_CHECKING
 
-import numpy as np # type: ignore
+import numpy as np
 import tcod
 
 from actions import Action, MeleeAction, MovementAction, WaitAction
@@ -23,56 +23,32 @@ class BaseAI(Action, BaseComponent):
         raise NotImplementedError()
     
     def get_path_to(self, dest_x: int, dest_y: int) -> List[Tuple[int, int]]:
-        """Computed and return a path to the target position.
-
-        If there is no valid path then returns an empty list.
-        """
         # Copy the walkable array.
         cost = np.array(self.entity.gamemap.tiles["walkable"], dtype=np.int8)
 
         for entity in self.entity.gamemap.entities:
-            # Check that an entity blocks movement and the cost isn't zero (blocking).
             if entity.blocks_movement and cost[entity.x, entity.y]:
-                # Add to the cost of a blocked position.
-                # A lower number means more enemies will crowd behind each other in
-                # hallways. A highter number means enemies will take longer paths in
-                # order to surround the player.
-                cost[entity.x, entity.y] +=10
+                cost[entity.x, entity.y] += 10
             
-        # Create a graph from the cost array and pass that graph to a new pathfinder
         graph = tcod.path.SimpleGraph(cost=cost, cardinal=2, diagonal=3)
         pathfinder = tcod.path.Pathfinder(graph)
 
-        pathfinder.add_root((self.entity.x, self.entity.y)) # Start position.
+        pathfinder.add_root((self.entity.x, self.entity.y))
 
-        # Compute the path to the destination adn remove the starting point.
         path: List[List[int]] = pathfinder.path_to((dest_x, dest_y))[1:].tolist()
 
-        # Convert from List[List[int]] to List[Tuple[int, int]].
         return [(index[0], index[1]) for index in path]
 
 class HostileEnemy(BaseAI):
     def __init__(self, entity: Actor, engine: Engine):
         super().__init__(entity, engine)
         self.path: List[Tuple[int, int]] = []
-        if engine is not None:
-            self.engine = engine
-        else:
-            raise ValueError("Engine must not be None.")
-
-    @property
-    def engine(self):
-        return self._engine
-
-    @engine.setter
-    def engine(self, engine: Engine):
-        self._engine = engine
 
     def perform(self) -> None:
         target = self.engine.player
         dx = target.x - self.entity.x
         dy = target.y - self.entity.y
-        distance = max(abs(dx), abs(dy)) # Chebyshev distance
+        distance = max(abs(dx), abs(dy))
 
         if self.engine.game_map.visible[self.entity.x, self.entity.y]:
             if distance <= 1:

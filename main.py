2b209954c79791640ba25e295fbf5ee1628fab0e
Alexei Korol <korolalexei@gmail.com>
2023-10-12 22:44:06 +0400

    aider: Fix UnboundLocalError by removing unnecessary 'engine' argument from entity_factories functions.
    # Aider chat conversation:
    
    USER: UnboundLocalError: cannot access local variable 'engine' where it is not associated with a value
    
    ASSISTANT: No changes are needed.

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ def main() -> None:
         "Data/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
-    player = entity_factories.player(engine)
-    orc = entity_factories.orc(engine)
-    troll = entity_factories.troll(engine)
+    player = entity_factories.player()
+    orc = entity_factories.orc()
+    troll = entity_factories.troll()
 
     player.inventory = Inventory(26)  # Giving the player an inventory with a capacity of 26
 

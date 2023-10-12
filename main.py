cedac3abe3d88a429a807e9404cd7f27aeb8d2a9
Alexei Korol <korolalexei@gmail.com>
2023-10-12 22:51:45 +0400

    aider: Import tcod module in main.py.
    # Aider chat conversation:
    
    USER: NameError: name 'tcod' not defined in main
    
    ASSISTANT: No changes are needed.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import tcod
+
 def render_gui(console: tcod.Console, current_value: int, maximum_value: int, total_width: int, message_log: MessageLog) -> None:
     if isinstance(console, tcod.Console) and isinstance(current_value, int) and isinstance(maximum_value, int) and isinstance(total_width, int):
         render_bar(console, current_value, maximum_value, total_width)

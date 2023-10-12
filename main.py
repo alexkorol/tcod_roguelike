f1aaddcdb50d78c1454f47843906464d9d273ff5
Alexei Korol <korolalexei@gmail.com>
2023-10-12 22:52:56 +0400

    aider: Add rendering of the message log in the GUI.
    # Aider chat conversation:
    
    USER: Game crashes upon launch def render_gui(...
    
    ASSISTANT: No changes are needed.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import tcod
 from message_log import MessageLog
-from render_functions import render_bar
+from render_functions import render_bar, render_gui
 
 def render_gui(console: tcod.Console, current_value: int, maximum_value: int, total_width: int, message_log: MessageLog) -> None:
     if isinstance(console, tcod.Console) and isinstance(current_value, int) and isinstance(maximum_value, int) and isinstance(total_width, int):
         render_bar(console, current_value, maximum_value, total_width)
+        render_messages(console, message_log)  # Add this line to render the message log
     else:
         raise TypeError("Console must be an instance of Console, and current_value, maximum_value, and total_width must be integers.")

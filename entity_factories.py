41e0bf44269ee21d0f8cd9f4d8e8b09242ec43cf
Alexei Korol <korolalexei@gmail.com>
2023-10-12 19:02:31 +0400

    aider: Added C:\Users\16178\Documents\Code\tcod_roguelike\entity_factories.py

--- entity_factories.py
+++ entity_factories.py
@@ -17,7 +17,7 @@ player = Actor(
     char="@",
     color=(255, 255, 255),
     name="Player",
-    ai_cls=HostileEnemy(engine),
+    ai_cls=HostileEnemy(Engine),
     fighter=Fighter(hp=30, defense=2, power=5),
     inventory=Inventory(26),
     render_order=RenderOrder.ACTOR
@@ -27,7 +27,7 @@ orc = Actor(
     char="o",
     color=(63, 127, 63),
     name="Orc",
-    ai_cls=HostileEnemy(engine),
+    ai_cls=HostileEnemy(Engine),
     fighter=Fighter(hp=10, defense=0, power=3),
     inventory=Inventory(0),
     render_order=RenderOrder.ACTOR
@@ -36,7 +36,7 @@ troll = Actor(
     char="T",
     color=(0, 127, 0),
     name="Troll",
-    ai_cls=HostileEnemy(engine),
+    ai_cls=HostileEnemy(Engine),
     fighter=Fighter(hp=16, defense=1, power=4),
     inventory=Inventory(0),
     render_order=RenderOrder.ACTOR
